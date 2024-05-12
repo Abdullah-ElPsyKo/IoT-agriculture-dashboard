0d174117ee330ca9a6dab91e06f1af50e367c4a5
Abdulla Bagishev <abdullahbagishev@icloud.com>
2024-05-12 15:26:59 +0200

    Generate real countries, cities, random dates

--- BACKEND/models/environmentalData.js
+++ BACKEND/models/environmentalData.js
@@ -1,5 +1,26 @@
 const { DataTypes } = require('sequelize');
 
+const countries = [
+  {
+    name: "United States",
+    cities: ["New York", "Los Angeles", "Chicago", "Houston"]
+  },
+  {
+    name: "United Kingdom",
+    cities: ["London", "Manchester", "Birmingham", "Glasgow"]
+  },
+  {
+    name: "France",
+    cities: ["Paris", "Marseille", "Lyon", "Toulouse"]
+  }
+];
+
+function generateRandomDate() {
+  const currentDate = new Date();
+  const pastDate = new Date(currentDate.getTime() - (365 * 24 * 60 * 60 * 1000)); // One year ago
+  return new Date(pastDate.getTime() + Math.random() * (currentDate.getTime() - pastDate.getTime()));
+}
+
 module.exports = (sequelize) => {
   if (!sequelize) {
     throw new Error('Sequelize instance was not passed to the model definition.');
@@ -7,6 +28,7 @@ module.exports = (sequelize) => {
 
   const EnvironmentalData = sequelize.define('EnvironmentalData', {
     date: DataTypes.DATE,
+    country: DataTypes.STRING,
     city: DataTypes.STRING,
     temperature: DataTypes.FLOAT,
     precipitation: DataTypes.FLOAT,
@@ -17,14 +39,19 @@ module.exports = (sequelize) => {
   });
 
   EnvironmentalData.insertInitialData = async function() {
-    const data = [...Array(500)].map(() => ({
-      date: new Date(),
-      city: "Sample City",
-      temperature: Math.random() * 35,
-      precipitation: Math.random() * 100,
-      soilMoisture: Math.random() * 100,
-      winds: Math.random() * 100
-    }));
+    const data = [...Array(500)].map(() => {
+      const country = countries[Math.floor(Math.random() * countries.length)];
+      const city = country.cities[Math.floor(Math.random() * country.cities.length)];
+      return {
+        date: generateRandomDate(),
+        country: country.name,
+        city: city,
+        temperature: Math.random() * 35,
+        precipitation: Math.random() * 100,
+        soilMoisture: Math.random() * 100,
+        winds: Math.random() * 100
+      };
+    });
     await this.bulkCreate(data);
     console.log('Initial data inserted successfully!');
   };
