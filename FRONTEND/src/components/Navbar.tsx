dd8c45f57cfa89af57a7fb55a3e04bc042a85ae6
John von Muhlen <johnvonmuhlen@gmail.com>
2024-05-28 13:47:51 +0200

    add links to navbar

--- FRONTEND/src/components/Navbar.tsx
+++ FRONTEND/src/components/Navbar.tsx
@@ -12,12 +12,10 @@ import {
   Transition,
 } from "@headlessui/react";
 import {
-  ArrowPathIcon,
   Bars3Icon,
   ChartPieIcon,
   CursorArrowRaysIcon,
   FingerPrintIcon,
-  SquaresPlusIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
@@ -38,39 +36,6 @@ const didYouKnowItems = [
   {
     name: "Africa Needs To Act Now",
     description: "Your customers’ data will be safe and secure",
-    href: "#",
-    icon: FingerPrintIcon,
-  },
-];
-
-const aboutUsItems = [
-  {
-    name: "Our Project",
-    description: "Get a better understanding of your traffic",
-    href: "#",
-    icon: ArrowPathIcon,
-  },
-  {
-    name: "Our Mission",
-    description: "Speak directly to your customers",
-    href: "#",
-    icon: SquaresPlusIcon,
-  },
-  {
-    name: "Our Vision",
-    description: "Your customers’ data will be safe and secure",
-    href: "#",
-    icon: FingerPrintIcon,
-  },
-  {
-    name: "Our Values",
-    description: "Your customers’ data will be safe and secure",
-    href: "#",
-    icon: FingerPrintIcon,
-  },
-  {
-    name: "Promoters",
-    description: "Our team and partners",
     href: "#",
     icon: FingerPrintIcon,
   },
@@ -115,6 +80,9 @@ const Navbar = () => {
           >
             Weather Radar
           </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-white">
+            Crop Recommendation
+          </a>
           <Popover className="relative">
             <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
               Do You Know?
@@ -132,7 +100,7 @@ const Navbar = () => {
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 translate-y-1"
             >
-              <PopoverPanel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+              <PopoverPanel className="absolute left-0 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transform -translate-x-1/2">
                 <div className="p-4">
                   {didYouKnowItems.map((item) => (
                     <div
@@ -161,54 +129,8 @@ const Navbar = () => {
               </PopoverPanel>
             </Transition>
           </Popover>
-          <Popover className="relative">
-            <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-white">
-              About Us
-              <ChevronDownIcon
-                className="h-5 w-5 flex-none text-white"
-                aria-hidden="true"
-              />
-            </PopoverButton>
-
-            <Transition
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <PopoverPanel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {aboutUsItems.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon
-                          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                          aria-hidden="true"
-                        />
-                      </div>
-                      <div className="flex-auto">
-                        <a
-                          href={item.href}
-                          className="block font-semibold text-gray-900"
-                        >
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-              </PopoverPanel>
-            </Transition>
-          </Popover>
-          <a href="#" className="text-sm font-semibold leading-6 text-white">
-            TerraByte Solutions
+          <a href="" className="text-sm font-semibold leading-6 text-white">
+            About Us
           </a>
         </PopoverGroup>
       </nav>
