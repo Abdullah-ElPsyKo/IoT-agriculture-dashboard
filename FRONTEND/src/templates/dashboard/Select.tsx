112cc4f2be22777aa5ea6948ecb38fd9a1420e9e
John von Muhlen <johnvonmuhlen@gmail.com>
2024-05-30 12:47:52 +0200

    fix dropdown

--- FRONTEND/src/templates/dashboard/Select.tsx
+++ FRONTEND/src/templates/dashboard/Select.tsx
@@ -25,7 +25,7 @@ const CustomSelect = ({
       {({ open }) => (
         <>
           <div className="relative mt-2">
-            <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+            <ListboxButton className="relative w-48 cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
                 <span className="ml-3 block truncate">{selectedCity}</span>
               </span>
@@ -43,20 +43,19 @@ const CustomSelect = ({
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <ListboxOptions className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <ListboxOptions className="absolute z-10 mt-1 max-h-56 w-48 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {cities.map((city) => (
                   <ListboxOption
                     key={city}
-                    className={({ focus }) =>
+                    className={({ active }) =>
                       classNames(
-                        focus ? "bg-indigo-600 text-white" : "",
-                        !focus ? "text-gray-900" : "",
+                        active ? "bg-indigo-600 text-white" : "text-gray-900",
                         "relative cursor-default select-none py-2 pl-3 pr-9"
                       )
                     }
                     value={city}
                   >
-                    {({ selected, focus }) => (
+                    {({ selected, active }) => (
                       <>
                         <div className="flex items-center">
                           <span
@@ -72,7 +71,7 @@ const CustomSelect = ({
                         {selected ? (
                           <span
                             className={classNames(
-                              focus ? "text-white" : "text-indigo-600",
+                              active ? "text-white" : "text-indigo-600",
                               "absolute inset-y-0 right-0 flex items-center pr-4"
                             )}
                           >
